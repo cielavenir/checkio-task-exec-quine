15b4ab449be1d7365faff03f2a06a97ca5dd1211
cielavenir <cielartisan@gmail.com>
2014-08-07 10:48:57 +0900

    Initial

---
+++ verification/referee.py
@@ -0,0 +1,65 @@
+"""
+CheckiOReferee is a base referee for checking you code.
+    arguments:
+        tests -- the dict contains tests in the specific structure.
+            You can find an example in tests.py.
+        cover_code -- is a wrapper for the user function and additional operations before give data
+            in the user function. You can use some predefined codes from checkio.referee.cover_codes
+        checker -- is replacement for the default checking of an user function result. If given, then
+            instead simple "==" will be using the checker function which return tuple with result
+            (false or true) and some additional info (some message).
+            You can use some predefined codes from checkio.referee.checkers
+        add_allowed_modules -- additional module which will be allowed for your task.
+        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
+        remove_allowed_modules -- close standard library modules, as example "math"
+
+checkio.referee.checkers
+    checkers.float_comparison -- Checking function fabric for check result with float numbers.
+        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
+            digits after coma.
+
+checkio.referee.cover_codes
+    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
+        before user function calling, then using this function. For example: if your test's input
+        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
+    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
+
+"""
+
+from checkio.signals import ON_CONNECT
+from checkio import api
+from checkio.referees.io import CheckiOReferee
+from checkio.referees import cover_codes
+from checkio.referees import checkers
+
+from tests import TESTS
+
+class CheckioRefereeCode(CheckiOReferee):
+    def __init__(self, max_length, **kwargs):
+        self.max_length = max_length
+        super().__init__(**kwargs)
+    def check_current_test(self, data):
+        if self.inspector:
+            inspector_result, inspector_result_addon = self.inspector(self.code, self.runner)
+            self.inspector = None
+            self.current_test["inspector_result_addon"] = inspector_result_addon
+            if not inspector_result:
+                self.current_test["inspector_fail"] = True
+                api.request_write_ext(self.current_test)
+                return api.fail(0, inspector_result_addon)
+        #check_result = self.check_user_answer(user_result) #eh?
+        if data['result']==self.code:
+            return api.success(0)
+        else:
+		    return api.fail(0,'quine() returned different string.')
+
+api.add_listener(
+    ON_CONNECT,
+    CheckiORefereeCode(
+        tests=TESTS,
+        cover_code={
+            'python-27': cover_codes.unwrap_args,
+            'python-3': cover_codes.unwrap_args
+        },
+        function_name="quine",
+    ).on_ready)
---
+++ verification/tests.py
@@ -0,0 +1,17 @@
+"""
+TESTS is a dict with all you tests.
+Keys for this will be categories' names.
+Each test is dict with
+    "input" -- input data for user function
+    "answer" -- your right answer
+    "explanation" -- not necessary key, it's using for additional info in animation.
+"""
+
+TESTS = {
+    "Test": [
+        {
+            "input": [],
+            "answer": []
+        }
+    ]
+ }
